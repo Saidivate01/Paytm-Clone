196bc3aae25c72949953577351ac10792a403066
Saidivate01 <saisheshakd0102@gmail.com>
2024-12-28 17:35:00 +0530

    Delete Frontend directory

--- Frontend/Script.js
+++
@@ -1,9 +0,0 @@
-// Placeholder for handleLogin function
-function handleLogin() {
-    console.log("Login function to be implemented.");
-}
-
-// Placeholder for handleSubmit function
-function handleSubmit() {
-    console.log("Submit function to be implemented.");
-}
